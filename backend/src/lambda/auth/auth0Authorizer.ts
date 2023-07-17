b4f59e74cd060ceb27fca02f38a4688b2cabef65
Kuro <satefubuki@gmail.com>
2023-07-18 02:38:02 +0700

    Handle logic code

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -7,12 +7,13 @@ import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
-const logger = createLogger('auth')
+const logger = createLogger('authorization')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl =
+  'https://dev-phdh4lqgm4bkk87s.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,7 +62,29 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const resp = await Axios.get(jwksUrl)
+  const respKeys = resp.data.keys
+  const signinKey = await getSigninkey(respKeys, jwt)
+  logger.info(`Signinkey: `, signinKey)
+  const cert = await converPEMtoCert(signinKey.x5c[0])
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+}
+
+function getSigninkey(keys, jwt) {
+  if (!keys) {
+    console.log('Invalid keys or not exists')
+  }
+  return keys.find((key) => key.kid === jwt.header.kid)
+}
+
+function converPEMtoCert(pemData) {
+  let cert = ''
+  if (!pemData) {
+    console.log('Pem is invalid keys or not exists')
+  }
+  cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----\n`
+  logger.info('cert: ', cert)
+  return cert
 }
 
 function getToken(authHeader: string): string {
